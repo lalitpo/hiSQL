044ed5115ce09ddefee124f0236db525af0e2ddc
shantanu-kulkarni <shantanukulkarni73@gmail.com>
2022-02-04 16:56:42 +0100

    Adding enumeration examples

--- hiSQL2.sql
+++ hiSQL2.sql
@@ -344,8 +344,8 @@ WHERE  (holiday.start :: date, holiday.end :: date) overlaps ('today','today');
 -- Syntax:
 
 -- CREATE TYPE <t> AS ENUM(<v1>, <v2>,..., <vn>);
-SELECT <vi>::<t>;
-where vi is a case sensitive notation and stored in 4 bytes.
+-- SELECT <vi>::<t>;
+-- where vi is a case sensitive notation and stored in 4 bytes.
 
 -- Example:
 
@@ -353,8 +353,36 @@ DROP TYPE IF EXISTS episode CASCADE;
 CREATE TYPE episode AS ENUM
   ('ANH', 'ESB', 'TPM', 'AOTC', 'ROTS', 'ROTJ', 'TFA', 'TLJ', 'TROS');
 
+DROP TABLE IF EXISTS starwars;
+CREATE TABLE starwars(film    episode PRIMARY KEY,
+                      title   text,
+                      release date);
+
+INSERT INTO starwars(film,title,release) VALUES
+    ('TPM',  'The Phantom Menace',      'May 19, 1999'),
+    ('AOTC', 'Attack of the Clones',    'May 16, 2002'),
+    ('ROTS', 'Revenge of the Sith',     'May 19, 2005'),
+    ('ANH',  'A New Hope',              'May 25, 1977'),
+    ('ESB',  'The Empire Strikes Back', 'May 21, 1980'),
+    ('ROTJ', 'Return of the Jedi',      'May 25, 1983'),
+    ('TFA',  'The Force Awakens',       'Dec 18, 2015'),
+    ('TLJ',  'The Last Jedi',           'Dec 15, 2017'),
+    ('TROS', 'The Rise of Skywalker',   'Dec 19, 2019');
+--     ↑              ↑                        ↑
+-- ::episode       ::text                    ::date
+
+INSERT INTO starwars(film,title,release) VALUES
+  ('R1', 'Rogue One', 'Dec 15, 2016');
+--   ↑
+-- ⚠ not an episode value - throws error
+
+-- Order of enumerated type yields the Star Wars Machete order
+SELECT s.*
+FROM   starwars AS s
+ORDER BY s.film; -- s.release; -- yields chronological order
+
 ----------------------------------------------------------------------------------------------------
--- Video 17 : Bit strings (bit(n)), BLOBS, and byte arrays (bytes)
+-- Bit strings (bit(n)), BLOBS, and byte arrays (bytes)
 ----------------------------------------------------------------------------------------------------
 
 -- Bit strings : 
