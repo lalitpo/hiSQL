7f78c510084af700a43edb517ebb48a5430c7e7e
shantanu-kulkarni <shantanukulkarni73@gmail.com>
2022-02-05 00:16:47 +0100

    Add JSON examples

--- hiSQL2.sql
+++ hiSQL2.sql
@@ -655,7 +655,107 @@ WHERE  circle(point(0.5,0.5), 0.5) @> point(random(),random());
 -- JSON support (type jsonb) [and XML support]
 ----------------------------------------------------------------------------------------------------
 
--- 
+-- JSON defines a textual data interchange format which is easy for humans to write and machines
+-- to parse
+
+-- JSON Documentation : http://json.org/
+
+-- SQL:2016 defines SQL↔JSON interoperability. JSON values may be constructed/traversed and 
+-- held in table cells (we still consider 1NF to be intact).
+
+-- jsonb
+VALUES (1, '{ "b":1, "a":2 }'       ::jsonb),  -- ← pair order may flip
+       (2, '{ "a":1, "b":2, "a":3 }'       ),  -- ← duplicate field
+       (3, '[ 0,   false,null ]'           );  -- ← whitespace normalized
+
+-- json
+VALUES (1, '{ "b":1, "a":2 }'       ::json ),  -- ← pair order and ...
+       (2, '{ "a":1, "b":2, "a":3 }'       ),  -- ← ... duplicates preserved
+       (3, '[ 0,   false,null ]'           );  -- ← whitespace preserved
+
+
+
+-- Navigating in an extracting from a JSON value
+SELECT ('{ "a":0, "b": { "b₁":1, "b₂":2 } }' :: jsonb -> 'b' ->> 'b₂')::int + 40;
+--                                                            ↑
+--                                       extract as text (cannot cast jsonb to τ)
+
+
+-------------------------------
+-- Goal: Convert table into JSON (jsonb) array of objects
+
+-- Step ➊: convert each row into a JSON object (columns ≡ fields)
+--
+SELECT row_to_json(t)::jsonb
+FROM   T AS t;
+
+-- Step ➋: aggregate the table of JSON objects into one JSON array
+--          (here: in some element order)
+--
+--  may understood as a unity for now (array_agg() in focus soon)
+--     ╭──────────┴───────────╮
+SELECT array_to_json(array_agg(row_to_json(t)))::jsonb
+FROM   T as t;
+
+
+-- Pretty-print JSON output
+--
+SELECT jsonb_pretty(array_to_json(array_agg(row_to_json(t)))::jsonb)
+FROM   T as t;
+
+
+-- Table T represented as a JSON object (JSON value in single row/single column)
+DROP TABLE IF EXISTS like_T_but_as_JSON;
+CREATE TEMPORARY TABLE like_T_but_as_JSON(a) AS
+  SELECT array_to_json(array_agg(row_to_json(t)))::jsonb
+  FROM   T as t;
+
+TABLE like_T_but_as_JSON;
+
+-------------------------------
+-- Goal: Convert JSON object (array of regular objects) into a table:
+--       can we do a round-trip and get back the original T?
+
+
+-- Step ➊: convert array into table of JSON objects
+--
+SELECT o
+FROM   jsonb_array_elements((TABLE like_T_but_as_JSON)) AS objs(o);
+
+-- NB: Steps ➋a and ➋b/c lead to alternative tabular representation:
+
+-- Step ➋a: turn JSON objects into key/value pairs (⚠ column value::jsonb)
+--
+SELECT t.*
+FROM   jsonb_array_elements((TABLE like_T_but_as_JSON)) AS objs(o),
+       jsonb_each(o) AS t;
+
+
+-- Step ➋b: turn JSON objects into rows (fields ≡ columns)
+--
+SELECT t.*
+FROM   jsonb_array_elements((TABLE like_T_but_as_JSON)) AS objs(o),
+       jsonb_to_record(o) AS t(a int, b text, c boolean, d int);
+--                           ╰────────────────┬───────────────╯
+--                   explicitly provide column name and type information
+--                      (⚠ column and field names/types must match)
+
+
+-- Step ➋c: turn JSON objects into rows (fields ≡ columns)
+--
+SELECT t.*
+FROM   jsonb_array_elements((TABLE like_T_but_as_JSON)) AS objs(o),
+       jsonb_populate_record(NULL :: T, o) AS t;
+--                          ╰───┬────╯
+--   derive column names and types from T's row type (cf. Chapter 02)
+--            (⚠ column and field names/types must match)
+
+
+
+-- Steps ➊+➋: from array of JSON objects directly to typed tables
+SELECT t.*
+FROM   jsonb_populate_recordset(NULL :: T, (TABLE like_T_but_as_JSON)) AS t;
+
 
 ----------------------------------------------------------------------------------------------------
 --  
